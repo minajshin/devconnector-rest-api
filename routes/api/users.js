ebcfe33799af4ade3012f8c07a4d30767fac735f
Mina Shin <me@minashin.net>
2020-08-06 01:36:27 -0700

    Validate request body for registering user

---
+++ routes/api/users.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ * @access Public
+ */
+router.post(
+  '/',
+  [
+    body('name', 'Name is required').not().isEmpty(),
+    body('email', 'Email is required').isEmail(),
+    body(
+      'password',
+      'Password is required with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('User Registered');
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const connectDB = require('./config/db');
 const app = express();
 connectDB();
 
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Server Running'));
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
